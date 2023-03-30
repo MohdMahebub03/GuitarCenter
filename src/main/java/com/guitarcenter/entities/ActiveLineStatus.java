4cd897af27067aa3632009f3d6d86988060c4c24
tahseen.m <tahseen.m@xitsolutionsinc.com>
2023-03-30 14:56:02 +0530

    Added Comment

--- src/main/java/com/guitarcenter/entities/ActiveLineStatus.java
+++ src/main/java/com/guitarcenter/entities/ActiveLineStatus.java
@@ -1,5 +1,10 @@
 package com.guitarcenter.entities;
 
+import org.springframework.data.cassandra.core.mapping.Table;
+
+@Table(value="active_line_status")
 public class ActiveLineStatus {
+	
+	//testing github
 
 }
